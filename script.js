23c0849d84f0ebdb2eb3af0bf08c48256eb1d815
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-10-09 15:44:59 +0100

    add click event to radio button

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const gamePage = document.getElementById('game-page');
 const scorePage = document.getElementById('score-page');
 const splashPage = document.getElementById('splash-page');
 const countdownPage = document.getElementById('countdown-page');
+
 // Splash Page
 const startForm = document.getElementById('start-form');
 const radioContainers = document.querySelectorAll('.radio-container');
 const radioInputs = document.querySelectorAll('input');
 const bestScores = document.querySelectorAll('.best-score-value');
+
 // Countdown Page
 const countdown = document.querySelector('.countdown');
+
 // Game Page
 const itemContainer = document.querySelector('.item-container');
+
 // Score Page
 const finalTimeEl = document.querySelector('.final-time');
 const baseTimeEl = document.querySelector('.base-time');
@@ -33,7 +37,7 @@ const wrongFormat = [];
 // Scroll
 
 // Create Correct/Incorrect Random Equations
-function createEquations() {
+const createEquations = () => {
   // Randomly choose how many correct equations there should be
   // const correctEquations = 
   // Set amount of wrong equations
@@ -82,3 +86,15 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+startForm.addEventListener('click', () => {
+  radioContainers.forEach(radioEl => {
+    // Remove Selected Label Styling
+    radioEl.classList.remove('selected-label');
+
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
